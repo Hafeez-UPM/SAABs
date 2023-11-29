8643a3715da38c7bc5801c9956ccb4f9076f7825
Hafeez-UPM <77595182+Hafeez-UPM@users.noreply.github.com>
2023-11-29 10:22:06 +0800

    add files

---
+++ rversedesigner/src/rversedesigner/REMetrics.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rversedesigner;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import com.sdmetrics.model.MetaModel;
+import com.sdmetrics.model.MetaModelElement;
+import com.sdmetrics.model.Model;
+import com.sdmetrics.model.ModelElement;
+import com.sdmetrics.model.XMIReader;
+import com.sdmetrics.model.XMITransformations;
+import com.sdmetrics.util.XMLParser;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Action;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+/**
+ *
+ * @author aislimau
+ */
+public class REMetrics {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
